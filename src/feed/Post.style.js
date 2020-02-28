63f88f41049f2045bb0bad4f13637b7e8a865753
giwiro <giwirodavalos@gmail.com>
2020-02-28 08:31:19 -0500

    first commit

---
+++ src/feed/Post.style.js
@@ -0,0 +1,20 @@
+// @flow
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 12,
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
