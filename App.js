a76b287d79527e277443f3659eb88c41481a528c
giwiro <giwirodavalos@gmail.com>
2020-03-06 00:02:47 -0500

    added navigation

--- App.js
+++ App.js
@@ -5,11 +5,12 @@
  * @format
  * @flow
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import FeedContainer from './src/modules/feed/containers/FeedContainer';
 import ErrorContainer from './src/modules/error/containers/ErrorContainer';
 import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
 import configureStore from './src/store';
 
 // Here put the initialState persisted
@@ -18,12 +19,12 @@ const store = configureStore(initialState);
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <Provider store={store}>
         <ErrorContainer />
         <FeedContainer />
       </Provider>
-    </>
+    </NavigationContainer>
   );
 };
 
